d1921c66aafb3a954fc5757f9fa56f3741b0f60c
AStaniszewski <staniszewski01adam@gmail.com>
2024-01-18 16:09:59 +0100

    16.01

--- Game-of-Life/patterns.py
+++ Game-of-Life/patterns.py
@@ -1,6 +1,19 @@
 from dataclasses import dataclass
 
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomlib
+
 @dataclass
 class Pattern:
     name: str
     alive_cells: set[tuple[int, int]]
+
+    @classmethod
+    def from_toml(cls, name, toml_data):
+        return cls(
+            name,
+            alive_cells={tuple(cell) for cell in toml_data["alive_cells"]},
+        )
+
